d2edd0e93cc58623d9bb285d4fad31bc41997851
William Gotterer <william.gotterer@gmail.com>
2021-11-30 17:13:48 -0500

    new buttons

--- src/components/Work.js
+++ src/components/Work.js
@@ -75,13 +75,13 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
         <Typography className="grid_item" variant="h4">
         The map() function is quintessential when working with JavaScript or other frameworks. I aim to explain the basics of how this function works with the help of Spongebob and his friends. 
            <br /> <Button onClick={handleMap}  target="_blank" variant="contained" color="primary">
-        map()
+        Read More
         </Button>
         </Typography>
         <Typography className="grid_item" variant="h4">
         “Spice It Up!” is an application made with React. A user randomly receives a recipe from an API and can check out various features that pertain to that recipe. Sometimes it’s hard to choose your next meal. “Spice It Up!” does it for you.
            <br /> <Button onClick={handleSpice}  target="_blank" variant="contained" color="primary">
-        Spice it Up!
+        Demo
         </Button>
         </Typography>
         </ThemeProvider>
@@ -91,13 +91,13 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
         <Typography className="grid_item" variant="h4">
         This blog aims to help a beginner Rubyist take the first steps in creating tables, using Active Record, SQL, and Sinatra, rake commands, and ultimately have a solid platform to take the nexts steps in building an API. 
             <br /> <Button onClick={handleTable}  target="_blank" variant="contained" color="primary">
-        Tables Blog
+        Read More
         </Button>
         </Typography>
         <Typography className="grid_item" variant="h4">
         “Monster Code” is an app built with React and Ruby. Structured in a similar fashion to an arcade game,  a user must answer code related questions in order to fight and collect all the monsters. The following videos will give a detailed overview of the project and subsequently how and why it was coded the way it was. 
            <br /> <Button onClick={handleMonster}  target="_blank" variant="contained" color="primary">
-        Monster Code
+        Watch
         </Button>
         </Typography>
         </ThemeProvider>
@@ -107,14 +107,14 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
         <Typography className="grid_item" variant="h4">
         Knowing how to effectively use props in React is key to understanding the functionality of the framework. This blog aims to teach how props work and how information can be passed back up to parent components. Told in the form of a fairy tale. 
            <br /> <Button onClick={handleProps}  target="_blank" variant="contained" color="primary">
-        props
+        Read More
         </Button>
         </Typography>
         </ThemeProvider>
         <ThemeProvider theme={theme}>
         <Typography className="grid_item" variant="h4">
         Using vanilla JavaScript, “Lost Languages” was created. A random picture is generated from an external API and a user is then prompted to try and write a sentence about the picture using their target language and their first language. One can choose a variety of languages to practice as well as add translations to an existing picture.           <br /> <Button onClick={handleLang}  target="_blank" variant="contained" color="primary">
-        props
+        Demo
         </Button>
         </Typography>
         </ThemeProvider>
