ea7fb4d26d2fe616d372dee3a48a55fe1e396e55
William Gotterer <william.gotterer@gmail.com>
2021-11-30 08:59:50 -0500

    button additions

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,19 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
         history.push("/about")
     }
 
+    function handleLinkedin(){
+        window.open('https://www.linkedin.com/in/william-gotterer-a8581988/')
+    }
+    function handleDev(){
+        window.open('https://dev.to/wgotterer')
+    }
+
+    function handleGit(){
+        window.open('https://github.com/wgotterer?tab=repositories')
+    }
+
+   
+
     const theme = createTheme({
         typography: {
             h4: {
@@ -50,10 +63,22 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
         <Button onClick={handleLearnMore}  variant="contained" color="primary">
         Learn More
         </Button>
-        <Typography variant="h2">
-        <img height="50" width="50" alt="linkedin" src="https://cdn.iconscout.com/icon/free/png-256/linkedin-3691226-3073746.png"/>
+        <Typography className="home_button" variant="h2">
+        <a class="mailto" href="mailto:william.gotterer@gmail.com">
+        <img height="50" width="50" alt="mail" src="https://i.pinimg.com/originals/ca/27/f6/ca27f615c790f1af43f2dd564599bf6a.jpg"/>
+        </a>
+        </Typography>
+        <Typography className="home_button" variant="h2">
+        <img onClick={handleLinkedin} height="50" width="50" alt="linkedin" src="https://cdn.iconscout.com/icon/free/png-256/linkedin-3691226-3073746.png"/>
+        </Typography>
+        <Typography className="home_button" variant="h2">
+        <img onClick={handleDev} height="50" width="50" alt="Git Hub" src="https://newsignature.com/wp-content/uploads/2020/08/github-logo-tile.png"/>
+        </Typography>
+        <Typography className="home_button" variant="h2">
+        <img onClick={handleGit} height="50" width="50" alt="Dev" src="https://d2fltix0v2e0sb.cloudfront.net/dev-black.png"/>
         </Typography>
         </div>
+        
         </ThemeProvider>
         </div>
     )
