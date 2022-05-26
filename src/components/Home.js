d59f226e171ce5178c0f59d74edf97ecc7613661
William Gotterer <william.gotterer@gmail.com>
2022-05-26 11:32:55 -0500

    added header styled components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,94 +1,141 @@
-import React from 'react'
+import React from "react";
 import { useHistory } from "react-router-dom";
-import Button from '@mui/material/Button';
-import '@fontsource/roboto';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Work from "./Work"
+import Button from "@mui/material/Button";
+import "@fontsource/roboto";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Work from "./Work";
+import styled from "styled-components";
 
+function Home() {
+  let history = useHistory();
 
+  function handleLearnMore() {
+    history.push("/about");
+  }
 
+  function handleLinkedin() {
+    window.open("https://www.linkedin.com/in/william-gotterer-a8581988/");
+  }
+  function handleDev() {
+    window.open("https://dev.to/wgotterer");
+  }
 
- function Home() {
+  function handleGit() {
+    window.open("https://github.com/wgotterer");
+  }
 
-    let history = useHistory();
-
-    function handleLearnMore(){
-        history.push("/about")
-    }
-
-    function handleLinkedin(){
-        window.open('https://www.linkedin.com/in/william-gotterer-a8581988/')
-    }
-    function handleDev(){
-        window.open('https://dev.to/wgotterer')
-    }
-
-    function handleGit(){
-        window.open('https://github.com/wgotterer')
-    }
-
-   
-
-    const theme = createTheme({
-        typography: {
-            h4: {
-                fontSize: 25,
-                marginBottom: 30,
-            },
-            h2: {
-                
-                marginBottom: 30,
-            }
-        },
-        palette: {
-            primary: {
-              // Purple and green play nicely together.
-              main: '#ebf8e1',
-            }
-        }
-      });
-
-    return (
-        // <div className="home_container">
-       <div>
-        <ThemeProvider theme={theme}>
-        <div className="home_text_container">
-        <Typography variant="h4">
-            Hi, I'm William.
-        </Typography>
+  return (
+    // <div className="home_container">
+    //    <div>
+    //     <ThemeProvider theme={theme}>
+    //     <div className="home_text_container">
+    //     <Typography variant="h4">
+    //         Hi, I'm William.
+    //     </Typography>
+    //     <Typography variant="h2">
+    //     Software Engineer. Teacher. Musician.
+    //     </Typography>
+    //     <Typography variant="h4">
+    //     Full Stack Software Engineer with a passion for innovation and creating a better world.
+    //     </Typography>
+    //     <Button onClick={handleLearnMore}  variant="contained" color="primary">
+    //     Learn More
+    //     </Button>
+    //     <div className="button_container">
+    //     <Typography className="home_button" variant="h2">
+    //     <a class="mailto" href="mailto:william.gotterer@gmail.com">
+    //     <img height="50" width="50" alt="mail" src="https://seeklogo.com/images/G/gmail-icon-logo-9ADB17D3F3-seeklogo.com.png"/>
+    //     </a>
+    //     </Typography>
+    //     <Typography className="home_button" variant="h2">
+    //     <img onClick={handleLinkedin} height="50" width="50" alt="linkedin" src="https://cdn.iconscout.com/icon/free/png-256/linkedin-3691226-3073746.png"/>
+    //     </Typography>
+    //     <Typography className="home_button" variant="h2">
+    //     <img onClick={handleDev} height="50" width="50" alt="Dev" src="https://d2fltix0v2e0sb.cloudfront.net/dev-black.png" />
+    //     </Typography>
+    //     <Typography className="home_button" variant="h2">
+    //     <img onClick={handleGit} height="50" width="50" alt="Git" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" />
+    //     </Typography>
+    //     </div>
+    <HomeArea>
+      <HeaderArea>
+        <Typography variant="h4">Hi, I'm William.</Typography>
         <Typography variant="h2">
-        Software Engineer. Teacher. Musician.
+          Software Engineer. Teacher. Musician.
         </Typography>
         <Typography variant="h4">
-        Full Stack Software Engineer with a passion for innovation and creating a better world.
+          Full Stack Software Engineer with a passion for innovation and
+          creating a better world.
         </Typography>
-        <Button onClick={handleLearnMore}  variant="contained" color="primary">
-        Learn More
+        <Button onClick={handleLearnMore} variant="contained" color="primary">
+          Learn More
         </Button>
-        <div className="button_container">
-        <Typography className="home_button" variant="h2">
+      </HeaderArea>
+      <RedirectLinksArea>
+      <Typography className="home_button" variant="h2">
         <a class="mailto" href="mailto:william.gotterer@gmail.com">
-        <img height="50" width="50" alt="mail" src="https://seeklogo.com/images/G/gmail-icon-logo-9ADB17D3F3-seeklogo.com.png"/>
+          <img
+            height="50"
+            width="50"
+            alt="mail"
+            src="https://seeklogo.com/images/G/gmail-icon-logo-9ADB17D3F3-seeklogo.com.png"
+          />
         </a>
-        </Typography>
-        <Typography className="home_button" variant="h2">
-        <img onClick={handleLinkedin} height="50" width="50" alt="linkedin" src="https://cdn.iconscout.com/icon/free/png-256/linkedin-3691226-3073746.png"/>
-        </Typography>
-        <Typography className="home_button" variant="h2">
-        <img onClick={handleDev} height="50" width="50" alt="Dev" src="https://d2fltix0v2e0sb.cloudfront.net/dev-black.png" />
-        </Typography>
-        <Typography className="home_button" variant="h2">
-        <img onClick={handleGit} height="50" width="50" alt="Git" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" />
-        </Typography>
-        </div>
-        <Work />
-        </div>
-       
-        
-        </ThemeProvider>
-        </div>
-    )
+      </Typography>
+      <Typography className="home_button" variant="h2">
+        <img
+          onClick={handleLinkedin}
+          height="50"
+          width="50"
+          alt="linkedin"
+          src="https://cdn.iconscout.com/icon/free/png-256/linkedin-3691226-3073746.png"
+        />
+      </Typography>
+      <Typography className="home_button" variant="h2">
+        <img
+          onClick={handleDev}
+          height="50"
+          width="50"
+          alt="Dev"
+          src="https://d2fltix0v2e0sb.cloudfront.net/dev-black.png"
+        />
+      </Typography>
+      <Typography className="home_button" variant="h2">
+        <img
+          onClick={handleGit}
+          height="50"
+          width="50"
+          alt="Git"
+          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+        />
+      </Typography>
+      </RedirectLinksArea>
+      <Work />
+    </HomeArea>
+  );
 }
 
-export default Home
+const HomeArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  gap: 20px;
+`;
+
+const HeaderArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const RedirectLinksArea = styled.div`
+display: flex;
+flex-direction: row;
+width: 100%;
+justify-content: center;
+`
+
+export default Home;
